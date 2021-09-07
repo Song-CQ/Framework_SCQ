08380abb03980973f1224365509fedabe75c52de
相柳 <2728285639@qq.com>
2021-09-07 19:12:50 +0800

    提交更改

---
+++ Framework_UClient/Assets/_App/AutoCreator/ExcelClass_AutoCreator/Sheet1VO_AutoCreate/Sheet1VO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace ProjectApp.Data
+{
+    /// <summary>
+    /// Sheet1表 数据类
+    /// </summary>
+    public class Sheet1VO:BaseVO
+    {
+        
+        /// <summary>
+        /// 名字 
+        /// </summary>
+        public string name;
+
+    }
+}
+        
---
+++ Framework_UClient/Assets/_AppBase/Data/BaseExcelClass/BaseVO.cs
@@ -0,0 +1,13 @@
+namespace ProjectApp.Data
+{
+    public class BaseVO
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int id;
+
+
+
+    }
+}
