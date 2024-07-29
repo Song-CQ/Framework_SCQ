6566357d566ad5827eaeccd54443a00f10553e5e
DESKTOP-SE526FC\ASUS072 <2728285639@qq.com>
2024-07-29 15:00:51 +0800

    提交代码

---
+++ Framework_UClient/Assets/_App/ProjectApp/ProjectApp/Logic/ModuleUI/Tips/TipsModel.cs
@@ -0,0 +1,49 @@
+/****************************************************
+    文件: Model.cs
+    作者: Clear
+    日期: 2022/6/2 18:38:49
+    类型: MVC_AutoCread
+    功能: TipsModel
+*****************************************************/
+using FutureCore;
+
+namespace ProjectApp
+{
+    public class TipsModel:BaseModel
+    {
+
+        #region 生命周期
+        protected override void OnInit()
+        {
+        }
+
+        protected override void OnDispose()
+        {
+        }
+
+        protected override void OnReset()
+        {
+        }
+        #endregion
+
+        #region 读取数据
+        protected override void OnReadData()
+        {
+            
+        }
+
+        #endregion
+      
+        #region 消息
+        protected override void AddListener()
+        {
+            //modelDispatcher.AddListener(ModelMsg.XXX, OnXXX);
+        }
+        protected override void RemoveListener()
+        {
+            //modelDispatcher.RemoveListener(ModelMsg.XXX, OnXXX);
+        }
+        #endregion
+
+    }
+}
