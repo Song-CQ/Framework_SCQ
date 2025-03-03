61d6a2f39fc75ffc272b4327d705598efa17faa2
Song-CQ <81799064+Song-CQ@users.noreply.github.com>
2025-03-03 15:31:54 +0800

    Update CameraConst.cs

--- Framework_UClient/Assets/_FutureFrame/FutureCore/Define/Const/CameraConst.cs
+++ Framework_UClient/Assets/_FutureFrame/FutureCore/Define/Const/CameraConst.cs
@@ -10,22 +10,22 @@ using UnityEngine;
 
 namespace FutureCore
 {
-    public static class CameraConst 
+    public static class CameraConst
     {
-
         public const int MainDepth = 0;
-        public static float MainCameraFarClipPlane = 60;
-        public static TransparencySortMode MainCameraSortMode =  TransparencySortMode.Perspective;
-        public static int MainCameraOrthographicSize =  10;
-        public const int UICameraDepth = 10;
-        public static float UICameraFarClipPlane = 100;
+        public const TransparencySortMode MainCameraSortMode = TransparencySortMode.Perspective;
 
+        public static int MainCameraOrthographicSize = 10;
+        public static float MainCameraFarClipPlane = 60;
         public const int MainCameraPosValue = 0;
         public const int MainCameraZPos = 0;
+
+        public const int UICameraDepth = 10;
+        public static float UICameraFarClipPlane = 100;
         public const int UICameraPosValue = 10000;
 
         public static Vector3 MainCameraPos = new Vector3(MainCameraPosValue, MainCameraPosValue, MainCameraZPos);
         public static Vector3 UICameraPos = new Vector3(UICameraPosValue, UICameraPosValue, MainCameraZPos);
-        
+
     }
 }
